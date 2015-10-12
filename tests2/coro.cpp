7501749ad34e8f5a4b7c24f4f9c6151e939aea82
Ricardo Marmolejo Garcia <makiolo@gmail.com>
2015-10-12 22:06:56 +0200

    commented ssh code

--- tests2/coro.cpp
+++ tests2/coro.cpp
@@ -132,46 +132,46 @@ int main()
 		std::cout << line << std::endl;
 
 
-	ssh_session my_ssh_session;
-	int rc;
-	char* password;
-	// Open session and set options
-	my_ssh_session = ssh_new();
-	if (my_ssh_session == NULL)
-		exit(-1);
-	ssh_options_set(my_ssh_session, SSH_OPTIONS_HOST, "localhost");
-	// Connect to server
-	rc = ssh_connect(my_ssh_session);
-	if (rc != SSH_OK)
-	{
-		fprintf(stderr, "Error connecting to localhost: %s\n", ssh_get_error(my_ssh_session));
-		ssh_free(my_ssh_session);
-		exit(-1);
-	}
-	// Verify the server's identity
-	// For the source code of verify_knowhost(), check previous example
-	if (verify_knownhost(my_ssh_session) < 0)
-	{
-		ssh_disconnect(my_ssh_session);
-		ssh_free(my_ssh_session);
-		exit(-1);
-	}
-	// Authenticate ourselves
-	password = getpass("Password: ");
-	rc = ssh_userauth_password(my_ssh_session, NULL, password);
-	if (rc != SSH_AUTH_SUCCESS)
-	{
-		fprintf(stderr, "Error authenticating with password: %s\n", ssh_get_error(my_ssh_session));
-		ssh_disconnect(my_ssh_session);
-		ssh_free(my_ssh_session);
-		exit(-1);
-	}
-
-	std::cout << "connected!" << std::endl;
-	show_remote_processes(my_ssh_session);
-
-	ssh_disconnect(my_ssh_session);
-	ssh_free(my_ssh_session);
+	// ssh_session my_ssh_session;
+	// int rc;
+	// char* password;
+	// // Open session and set options
+	// my_ssh_session = ssh_new();
+	// if (my_ssh_session == NULL)
+	// 	exit(-1);
+	// ssh_options_set(my_ssh_session, SSH_OPTIONS_HOST, "localhost");
+	// // Connect to server
+	// rc = ssh_connect(my_ssh_session);
+	// if (rc != SSH_OK)
+	// {
+	// 	fprintf(stderr, "Error connecting to localhost: %s\n", ssh_get_error(my_ssh_session));
+	// 	ssh_free(my_ssh_session);
+	// 	exit(-1);
+	// }
+	// // Verify the server's identity
+	// // For the source code of verify_knowhost(), check previous example
+	// if (verify_knownhost(my_ssh_session) < 0)
+	// {
+	// 	ssh_disconnect(my_ssh_session);
+	// 	ssh_free(my_ssh_session);
+	// 	exit(-1);
+	// }
+	// // Authenticate ourselves
+	// password = getpass("Password: ");
+	// rc = ssh_userauth_password(my_ssh_session, NULL, password);
+	// if (rc != SSH_AUTH_SUCCESS)
+	// {
+	// 	fprintf(stderr, "Error authenticating with password: %s\n", ssh_get_error(my_ssh_session));
+	// 	ssh_disconnect(my_ssh_session);
+	// 	ssh_free(my_ssh_session);
+	// 	exit(-1);
+	// }
+    //
+	// std::cout << "connected!" << std::endl;
+	// show_remote_processes(my_ssh_session);
+    //
+	// ssh_disconnect(my_ssh_session);
+	// ssh_free(my_ssh_session);
 
 
 	//
