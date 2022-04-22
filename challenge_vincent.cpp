b3ada8e9a7237d7606a5eb5ac0b32b7ffccb8d6e
Ricardo <makiolo@gmail.com>
2022-04-22 13:44:43 +0200

    Update challenge_vincent.cpp

--- challenge_vincent.cpp
+++ challenge_vincent.cpp
@@ -3,71 +3,70 @@
 #include <functional>
 #include <type_traits>
 
-// C++17
-template <class F, class... Args>
-struct is_invocable
-{
-    template <class U>
-    static auto test(U* p) -> decltype((*p)(std::declval<Args>()...), void(), std::true_type());
-    template <class U>
-    static auto test(...) -> decltype(std::false_type());
-
-    static constexpr bool value = decltype(test<F>(0))::value;
-};
-
 class fun
 {
 public:
-	template <typename T, typename... Args>
-	void operator()(T&& input, Args&&... args) const
+	template <typename Tuple, typename T>
+	constexpr auto operator()(Tuple&& lambdas, T&& input) const
 	{
-	    bool executed = false;
-	    std::function<void(T&&)>* candidate = nullptr;
-		apply_fun_rec(executed, candidate, std::forward<Args>(args)...);
-		if(executed)
-		{
-		    candidate->operator()(std::forward<T>(input));
-		}
+	    return _unpack_tuple_a(std::forward<T>(input), std::forward<Tuple>(lambdas), std::make_index_sequence<std::tuple_size<std::remove_reference_t<Tuple>>::value>{});
 	}
-
 protected:
+	template <typename T, typename Tuple, std::size_t... S>
+	constexpr auto _unpack_tuple_a(T&& input, Tuple&& top, std::index_sequence<S...>) const
+	{
+		return _unpack_tuple_b(std::forward<T>(input), std::get<S>(std::forward<Tuple>(top))...);
+	}
+
+    template <typename T, typename... Args>
+    constexpr auto _unpack_tuple_b(T&& input, Args&&... args) const
+    {
+        return apply_fun_rec(input, std::forward<Args>(args)...);
+    }
+
     template <typename T, typename U>
-	void apply_fun(bool& executed, std::function<void(T&&)>* candidate, U&& x) const
-	{
-	    if(!executed)
-	    {
-	        candidate = std::bind(x);
-		    executed = true;
-	    }
-	}
+    constexpr auto apply_fun(T&& input, U&& x) const
+    {
+        return x(input); // apply functor
+    }
 
-	template <typename T, typename U, typename... Args>
-	void apply_fun_rec(bool& executed, std::function<void(T&&)>* candidate, U&& parm, Args&&... args) const
-	{
-		apply_fun(executed, candidate, std::forward<U>(parm));
-		apply_fun_rec(executed, candidate, std::forward<Args>(args)...);
-	}
+    template <typename T, typename U, typename... Args>
+    constexpr auto apply_fun_rec(T&& input, U&& parm, Args&&... args) const
+    {
+        if constexpr (std::is_convertible_v<U, std::function<void(T&&)> >)
+        {
+            return apply_fun(std::forward<T>(input), std::forward<U>(parm));
+        }
+        else
+        {
+            return apply_fun_rec(std::forward<T>(input), std::forward<Args>(args)...);
+        }
+    }
 
-	template <typename T, typename U>
-	void apply_fun_rec(bool& executed, std::function<void(T&&)>* candidate, U&& parm) const
-	{
-		apply_fun(executed, candidate, std::forward<U>(parm));
-	}
+    template <typename T, typename U>
+    constexpr auto apply_fun_rec(T&& input, U&& parm) const
+    {
+        if constexpr (std::is_convertible_v<U, std::function<void(T&&)> >)
+        {
+            return apply_fun(std::forward<T>(input), std::forward<U>(parm));
+        }
+    }
 };
 
 int main()
 {
-    fun h;
-    h(2, 
-    [](int data){
-        std::cout << data*2 << std::endl;
-    },
-    [](int data){
-        std::cout << data*4 << std::endl;
-    },
-    [](int data){
-        std::cout << data*8 << std::endl;
+    auto lambdas = std::make_tuple(
+    [](float data) {
+        std::cout << "float = " << data << std::endl;
+        return 1;
+    }, 
+    [](std::string data) {
+        std::cout << "string = " << data << std::endl;
+        return "pepe";
     });
-	return 0;
-}
 
+    fun foo;
+    std::cout << "ret = " << foo(lambdas, 1.2f) << std::endl;
+    std::cout << "ret = " << foo(lambdas, "hola") << std::endl;
+    return 0;
+}
