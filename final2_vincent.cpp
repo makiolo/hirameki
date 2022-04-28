b878307d33d5b9c0685cb5e7270fd414ba070ed3
Ricardo <makiolo@gmail.com>
2022-04-29 00:33:40 +0200

    Update final2_vincent.cpp

--- final2_vincent.cpp
+++ final2_vincent.cpp
@@ -2,54 +2,46 @@
 #include <vector>
 #include <typeinfo>
 #include <string>
-
+#include <memory>
 
 
 namespace gt
 {
 
+    namespace lambdas
+    {
 
+        namespace detail
+        {
 
-namespace lambdas
-{
+            template<typename ...F>
+            struct multi_lambda : public F...
+            {
+                multi_lambda(F &&...f) : F(std::forward<F>(f))... {}
+                using F::operator()...;
+            };
 
+        } // namespace detail
 
+        template <typename T>
+        std::shared_ptr<T> _make(T&& obj)
+        {
+            return std::make_shared<T>(std::forward<T>(obj));
+        }
 
-namespace detail
-{
+        template<typename ...F>
+        auto make(F &&...f) ->decltype(auto)
+        {
+            return _make(detail::multi_lambda<F...>(std::forward<F>(f)...));
+        }
 
-
-
-template<typename ...F>
-struct multi_lambda : public F...
-{
-multi_lambda(F &&...f) : F(std::forward<F>(f))... {}
-using F::operator()...;
-};
-
-
-
-
-} // namespace detail
-
-
-
-template<typename ...F>
-auto make(F &&...f) ->decltype(auto)
-{
-return detail::multi_lambda<F...>(std::forward<F>(f)...);
-}
-
-
-
-} // namespace lambdas
-
+    } // namespace lambdas
 
 
 } // namespace gt
 
-int main(){
-    auto multi = gt::lambdas::make (
+int main() {
+    auto multi = gt::lambdas::make(
         [](char) { return "char"; },
         [](const char*) { return "const char*"; },
         [](int) { return "int"; },
@@ -60,8 +52,8 @@ int main(){
         [](long long int) { return "long long int"; },
         [](auto) { return "unknown type"; }
     );
-    std::cout << multi("hola") << std::endl;
-    std::cout << multi(3) << std::endl;
-    std::cout << multi(3.2) << std::endl;
-    std::cout << multi(3.2f) << std::endl;
+    std::cout << (*multi)("hola") << std::endl;
+    std::cout << (*multi)(3) << std::endl;
+    std::cout << (*multi)(3.2) << std::endl;
+    std::cout << (*multi)(3.2f) << std::endl;
 }
